3efb844da79a85edb53bc3bb414b99d1f3b160b3
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2025-07-21 23:30:41 +0530

    concurrently package removed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/upload': 'http://localhost:5000',
-      '/files': 'http://localhost:5000',
-      '/uploads': 'http://localhost:5000',
-    },
-  },
+  build: {
+    outDir: path.resolve(__dirname, '../server/public'), // ⬅️ Move build output to server
+    emptyOutDir: true
+  }
 })
